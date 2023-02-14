23ddd9ddfc5850c816bc53fe7abf7c48ec93046b
profegil <profe.gilberto.ramirez@gmail.com>
2023-02-13 21:38:02 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+   <head>
+	<Title>Proyecto de muestra CIMAT</Title>
+        <meta charset="UTF-8"/>
+        <meta charset="iso-8859-1"/>
+        <link href="index.css" rel="stylesheet" type="text/css">
+   </head>
+   <body>
+   
+     <h2>
+    
+         <a class="titulo"> </a> 
+         <a class="boton" href="index.php">Inicio</a>
+         <a class="boton" href="genero.php">Consulta por género</a>
+         <a class="boton" href="edad.php">Consulta por edad</a>
+         
+     </h2>
+    
+	<center><br>
+ 	    <h1><br><br>Gilberto Antonio Ramírez Flores<br>
+	    Proyecto muestra para CIMAT</h1></font>
+            <br><br>
+            <h1>Bienvenido</h1>
+	<center>
+     </font>
+   </body>
+</html>
